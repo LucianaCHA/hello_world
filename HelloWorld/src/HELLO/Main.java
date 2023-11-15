0b65a05455bdf42c79122fa10d0c24ad3feaffe4
luciana <luciana@infraPC-0469>
2023-11-14 22:02:45 -0300

    Ejercicio clase 3 resuelto

--- HelloWorld/src/HELLO/Main.java
+++ HelloWorld/src/HELLO/Main.java
@@ -2,34 +2,38 @@ package HELLO;
 
 import java.util.ArrayList;
 import java.util.List;
-
-@FunctionalInterface
-interface Transform {
-	String toUpperCase(String word);
-}
+import java.util.stream.Collectors;
 
 public class Main {
-	// Metodo encargado de operar una lista de palabras
-	public static List<String> transformWordList(List<String> words, Transform transform) {
-		List<String> uppercasedList = new ArrayList();
 
-		// A cada palabra de la lista se le aplica el upper y se lo agrega a la lista
-		for (String word : words) {
-			uppercasedList.add(transform.toUpperCase(word));
-		}
-		return uppercasedList;
+	//EJERCICIO 1
+	//Implemente un método que reciba como parámetro una lista de strings y devuelva una nueva
+	//lista con los strings en mayúscula
+	public static List<String> StringListToUpperCase (List<String> words) {
+		return words.stream().map(word -> word.toUpperCase()).collect(Collectors.toList());
+		
 	}
-
+	
+	//EJERCICIO 2
+	//Implementar un método que reciba como parámetro una lista de strings y un entero "n". El
+	//método debe devolver un String que concatene separando por coma y espacio todas las
+	//palabras, en la lista, que tengan más de "n" caracteres.
+	
+	public static List<String> filterLongerThan(List<String> wordsList, int minLength){
+		
+		return wordsList.stream().filter(word-> word.length() > minLength).collect(Collectors.toList());
+	}
 	public static void main(String[] args) {
 		List<String> wordsList = List.of("hola", "soy", "luciana", "mate", "jengibre");
+		
+		List<String> wordsList2 = List.of("marie", "Curie", "RBert", "reid", "Salvat");
+		
+		
 
-		// Creo la interfaz funcional encargada de modificar las palabras
+		System.out.println(StringListToUpperCase(wordsList));
+		System.out.println(StringListToUpperCase(wordsList2));
+		
+		System.out.println(filterLongerThan(wordsList2, 4));
 
-		Transform toUpper = (word) -> word.toUpperCase();
-
-		// Llamo al metodo operar y le paso la lista de palabras para uppercasear
-
-		List<String> result = transformWordList(wordsList, toUpper);
-		System.out.println(result);
 	}
 }
